b212df8b66725a16d9e960e79a4778d01468defb
Marcia Castro <marcas4@gmail.com>
2021-11-10 16:48:20 -0500

    Se modificó team-table.component.html y team-table.component.ts donde vamos a ver la tabla del equipo con los jugadores, todavía las tablas están vacías, también se copió los estilos del profesor en el proyecto

--- src/app/team-table/team-table.component.ts
+++ src/app/team-table/team-table.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Country } from '../interfaces/player';
+import { Team } from '../interfaces/team';
+import { TeamService, TeamsTableHeaders } from '../services/team.service';
 
 @Component({
   selector: 'app-team-table',
   templateUrl: './team-table.component.html',
-  styleUrls: ['./team-table.component.scss']
+  styleUrls: ['./team-table.component.scss'],
 })
 export class TeamTableComponent implements OnInit {
-
-  constructor() { }
+  public teams$ = new Observable<Team[]>();
+  public tableHeaders = TeamsTableHeaders;
+  constructor(private teamService: TeamService) {}
 
   ngOnInit(): void {
+    this.teams$ = this.teamService.getTeam();
+    this.teamService
+      .getTeam()
+      .pipe(take(1))
+      .subscribe((teams) => {
+        if (teams.length === 0) {
+          const team: Team = {
+            name: 'MyAmazingTeam',
+            country: Country.Argentina,
+            player: null!
+          };
+        }
+      });
   }
-
 }
